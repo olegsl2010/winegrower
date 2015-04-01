b657c714d693a9f68bc0efe47e0155276060900b
olegsl2010 <olegsl2010@yahoo.com>
2015-04-01 23:50:05 +0300

    added reaktiv fragment

--- app/src/main/java/com/example/olegsl/winegrower/MainActivity.java
+++ app/src/main/java/com/example/olegsl/winegrower/MainActivity.java
@@ -1,22 +1,23 @@
 package com.example.olegsl.winegrower;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fragment = new PlaceholderFragment();
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -51,7 +52,7 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
         }
@@ -60,7 +61,16 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            Button reaktiv = (Button) rootView.findViewById(R.id.reaktiv);
+            reaktiv.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    FragmentTransaction FragManager = getSupportFragmentManager().beginTransaction();
+                    FragManager.replace(R.id.container, new Reaktiv());
+                    FragManager.commit();
+                }
+            });
             return rootView;
         }
+
     }
 }
