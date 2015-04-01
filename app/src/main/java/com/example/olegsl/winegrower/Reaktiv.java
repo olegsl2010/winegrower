ee27886b7197ddb70d51857e926f655ee877a1d0
olegsl2010 <olegsl2010@yahoo.com>
2015-04-01 23:59:09 +0300

    added reaktiv fragment

---
+++ app/src/main/java/com/example/olegsl/winegrower/Reaktiv.java
@@ -0,0 +1,22 @@
+package com.example.olegsl.winegrower;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Reaktiv extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView =
+                inflater.inflate(R.layout.reaktiv, container, false);
+
+        return rootView;
+    }
+
+}
