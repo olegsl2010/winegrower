ac90c499a452616762f0f494d0a376f64134e834
olegsl2010 <olegsl2010@yahoo.com>
2015-04-12 21:22:48 +0300

    edit reactiv raschet and celendar

---
+++ app/src/main/java/com/example/olegsl/winegrower/DatePickerDialogFragment.java
@@ -0,0 +1,93 @@
+package com.example.olegsl.winegrower;
+
+import java.util.Calendar;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.app.DialogFragment;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DatePickerDialogFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+    // определяем текущую дату
+    final Calendar c = Calendar.getInstance();
+    int year = c.get(Calendar.YEAR);
+    int month = c.get(Calendar.MONTH);
+    int day = c.get(Calendar.DAY_OF_MONTH);
+
+    // создаем DatePickerDialog и возвращаем его
+    Dialog picker = new DatePickerDialog(getActivity(), this,
+            year, month, day);
+    picker.setTitle(getResources().getString(R.string.choose_date));
+
+    return picker;
+}
+    @Override
+    public void onStart() {
+        super.onStart();
+        // добавляем кастомный текст для кнопки
+        Button nButton =  ((AlertDialog) getDialog())
+                .getButton(DialogInterface.BUTTON_POSITIVE);
+        nButton.setText(getResources().getString(R.string.ready));
+
+    }
+
+    @Override
+    public void onDateSet(android.widget.DatePicker datePicker, int year,
+                          int month, int day) {
+
+            if (MyCelendar.whatClick==1)
+            {
+                TextView tv = (TextView) getActivity().findViewById(R.id.tvDate1);
+                tv.setText(day + "-" + month + "-" + year);
+            }
+        if (MyCelendar.whatClick==2)
+        {
+            TextView tv = (TextView) getActivity().findViewById(R.id.tvDate2);
+            tv.setText(day + "-" + month + "-" + year);
+        }
+        if (MyCelendar.whatClick==3)
+        {
+            TextView tv = (TextView) getActivity().findViewById(R.id.tvDate3);
+            tv.setText(day + "-" + month + "-" + year);
+        }
+        if (MyCelendar.whatClick==4)
+        {
+            TextView tv = (TextView) getActivity().findViewById(R.id.tvDate4);
+            tv.setText(day + "-" + month + "-" + year);
+        }
+        if (MyCelendar.whatClick==5)
+        {
+            TextView tv = (TextView) getActivity().findViewById(R.id.tvDate5);
+            tv.setText(day + "-" + month + "-" + year);
+        }
+        if (MyCelendar.whatClick==6)
+        {
+            TextView tv = (TextView) getActivity().findViewById(R.id.tvDate6);
+            tv.setText(day + "-" + month + "-" + year);
+        }
+        if (MyCelendar.whatClick==7)
+        {
+            TextView tv = (TextView) getActivity().findViewById(R.id.tvDate7);
+            tv.setText(day + "-" + month + "-" + year);
+        }
+        if (MyCelendar.whatClick==8)
+        {
+            TextView tv = (TextView) getActivity().findViewById(R.id.tvDate8);
+            tv.setText(day + "-" + month + "-" + year);
+        }
+        if (MyCelendar.whatClick==9)
+        {
+            TextView tv = (TextView) getActivity().findViewById(R.id.tvDate9);
+            tv.setText(day + "-" + month + "-" + year);
+        }
+    }
+}
